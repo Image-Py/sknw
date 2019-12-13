962ad2d2a7bcf3c167f2dde6e53ba4355825fa56
yxdragon <imagepy@sina.com>
2019-12-13 12:14:40 +0800

    numba 4.5

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ Inspired by Juan Nunez-Iglesias's skan.
 
 if __name__ == '__main__':
     setup(name='sknw',
-        version='0.11',
+        version='0.13',
         url='https://github.com/yxdragon/sknw',
         description='Analysis of object skeletons',
         long_description=descr,
         author='YXDragon',
-        author_email='imagepyn@sina.com',
+        author_email='yxdragon@imagepy.org',
         license='BSD 3-clause',
         packages=['sknw'],
         package_data={},
